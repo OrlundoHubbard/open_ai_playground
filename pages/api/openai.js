5e7d146a02390efb047223050a8da2b85b1d7997
Orlundo Hubbard <orlundo@wursta.com>
2022-09-06 22:37:02 -0500

    work out error

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -18,8 +18,9 @@ export default async (_req, res) => {
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
   });
+  console.log(response);
 
-  res.status(200).json({ text: `${response.data.choices[0].text}` });
+  console.log(
+    res.status(200).json({ text: `${response.data.choices[0].text}` })
+  );
 };
-
-// "A neutron star is the collapsed core of a massive supergiant star, which had a total mass of between 10 and 25 solar masses, possibly more if the star was especially metal-rich.[1] Neutron stars are the smallest and densest stellar objects, excluding black holes and hypothetical white holes, quark stars, and strange stars.[2] Neutron stars have a radius on the order of 10 kilometres (6.2 mi) and a mass of about 1.4 solar masses.[3] They result from the supernova explosion of a massive star, combined with gravitational collapse, that compresses the core past white dwarf star density to that of atomic nuclei.\n\nTl;dr",
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,46 @@
 import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [data, setData] = useState({ text: "" });
+  const [query, setQuery] = useState();
+  const [search, setSearch] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      if (search) {
+        setIsLoading(true);
+        const res = await fetch(`/api/openai`, {
+          body: { name: search },
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    const response = await fetch("/api/openai")
-  }
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+        });
+        const data = await res.json();
+        setData(data);
+        setIsLoading(false);
+      }
+    };
+    // console.log(fetchData);
+    fetchData();
+  }, [search]);
+
   return (
     <>
       <form>
-        <textarea className={styles.textarea} id="summarization"></textarea>
-        <input type="submit" value="Submit" />
+        <input
+          type="text"
+          name="idea"
+          placeholder="Type any idea"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
       </form>
+      <button onClick={() => setSearch(query)}>Generate Idea</button>
+      <div>{isLoading ? <div>Loading...</div> : <span>{data.text}</span>}</div>
     </>
   );
 }
