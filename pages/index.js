148a4b50bd339ecf05ad361dccd0d2f8e3c82ffb
Roy Anger <roy@royanger.com>
2022-09-07 00:14:07 -0400

    Commited changes to component to resolve errors

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,49 @@
-import styles from "../styles/Home.module.css";
-import { useState, useEffect } from "react";
+import styles from '../styles/Home.module.css';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [data, setData] = useState({ text: "" });
-  const [query, setQuery] = useState();
-  const [search, setSearch] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+   const [data, setData] = useState({ text: '' });
+   const [query, setQuery] = useState('');
+   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (search) {
-        setIsLoading(true);
-        const res = await fetch(`/api/openai`, {
-          body: { name: search },
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      if (query) {
+         setIsLoading(true);
+         await fetch(`/api/openai`, {
+            body: JSON.stringify({ name: 'test' }),
 
-          headers: {
-            "Content-Type": "application/json",
-          },
-          method: "POST",
-        });
-        const data = await res.json();
-        setData(data);
-        setIsLoading(false);
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            method: 'POST',
+         })
+            .then(async (res) => {
+               const data = await res.json();
+               setData(data);
+               setIsLoading(false);
+            })
+            .catch((err) => {
+               throw new Error(`There was an error with the API: ${err}`);
+            });
       }
-    };
-    // console.log(fetchData);
-    fetchData();
-  }, [search]);
+   };
 
-  return (
-    <>
-      <form>
-        <input
-          type="text"
-          name="idea"
-          placeholder="Type any idea"
-          value={query}
-          onChange={(event) => setQuery(event.target.value)}
-        />
-      </form>
-      <button onClick={() => setSearch(query)}>Generate Idea</button>
-      <div>{isLoading ? <div>Loading...</div> : <span>{data.text}</span>}</div>
-    </>
-  );
+   return (
+      <>
+         <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+               type='text'
+               name='idea'
+               placeholder='Type any idea'
+               value={query}
+               onChange={(event) => setQuery(event.target.value)}
+            />
+            <button type='submit'>Generate Idea</button>
+         </form>
+         <div>
+            {isLoading ? <div>Loading...</div> : <span>{data.text}</span>}
+         </div>
+      </>
+   );
 }
